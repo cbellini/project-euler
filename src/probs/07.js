52b5798046c4d979555e0d5458f146ce54afee38
Chris Bellini <chris@chrisbellini.com>
2018-02-17 14:59:02 -0500

    initial solution to problem 7

--- src/probs/07.js
+++ src/probs/07.js
@@ -1,7 +1,37 @@
 // https://projecteuler.net/problem=7
-
 exports.solve = function (...args) {
     var primeToFind = args[0] || 10001;
 
-    // TODO
+    function* primes() {
+        let n = 2;
+
+        while (true) {
+          if (isPrime(n)) yield n;
+          n++;
+        }
+
+        function isPrime(num) {
+          for (var i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+              return false;
+            }
+          }
+          return true;
+        }
+    }
+
+    function* take(limit, iterable) {
+        for (let x of iterable) {
+          if (limit <= 0){
+            return;
+          }
+
+          limit--;
+          yield x;
+        }
+    }
+
+    var primes = [...take(primeToFind, primes())];
+
+    return primes.slice(-1)[0];
 }
--- test/07.test.js
+++ test/07.test.js
@@ -1,11 +1,13 @@
 var assert = require('assert');
 
+
+
 describe('Euler Problem 7 Tests', function() {
   describe('Solve function', function() {
-    it('should return TODO as the 10001st prime number', function() {
+    it('should return 104743 as the 10001st prime number', function() {
       var solver = require('../src/probs/07.js');
       var result = solver.solve.apply(null, [10001]);
-      // TODO
+      assert.equal(result, 104743);
     });
   });
 });
