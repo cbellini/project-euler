0361f26a9d154cb4f689a47eea12b6659b5a101c
Chris Bellini <chris@chrisbellini.com>
2018-02-17 23:20:43 -0500

    fixed eslint warnings

--- src/probs/07.js
+++ src/probs/07.js
@@ -1,7 +1,10 @@
 // https://projecteuler.net/problem=7
-exports.solve = function (...args) {
-    var primeToFind = args[0] || 10001;
+exports.solve = function(...args) {
+    let primeToFind = args[0] || 10001;
 
+    /**
+    * Generate prime numbers.
+    */
     function* primes() {
         let n = 2;
 
@@ -10,8 +13,13 @@ exports.solve = function (...args) {
           n++;
         }
 
+        /**
+         * Add two numbers.
+         * @param {number} num The input number.
+         * @return {boolean} Return true if the number is prime.
+         */
         function isPrime(num) {
-          for (var i = 2; i <= Math.sqrt(num); i++) {
+          for (let i = 2; i <= Math.sqrt(num); i++) {
             if (num % i === 0) {
               return false;
             }
@@ -20,9 +28,15 @@ exports.solve = function (...args) {
         }
     }
 
+    /**
+     * Generate results from a generator function.
+     * @param {number} limit The number of results to generate.
+     * @param {function} iterable The generator function.
+     * @yield {array} List of generated results.
+     */
     function* take(limit, iterable) {
         for (let x of iterable) {
-          if (limit <= 0){
+          if (limit <= 0) {
             return;
           }
 
@@ -31,7 +45,7 @@ exports.solve = function (...args) {
         }
     }
 
-    var primes = [...take(primeToFind, primes())];
+    let primeNumbers = [...take(primeToFind, primes())];
 
-    return primes.slice(-1)[0];
-}
+    return primeNumbers.slice(-1)[0];
+};
--- test/07.test.js
+++ test/07.test.js
@@ -1,12 +1,10 @@
-var assert = require('assert');
-
-
+let assert = require('assert');
 
 describe('Euler Problem 7 Tests', function() {
   describe('Solve function', function() {
     it('should return 104743 as the 10001st prime number', function() {
-      var solver = require('../src/probs/07.js');
-      var result = solver.solve.apply(null, [10001]);
+      let solver = require('../src/probs/07.js');
+      let result = solver.solve.apply(null, [10001]);
       assert.equal(result, 104743);
     });
   });
