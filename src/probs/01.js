bdb19f8d33c3b9acf0845e44d426657c440a6b39
Chris Bellini <chris@chrisbellini.com>
2018-02-17 23:23:07 -0500

    fixed eslint warnings

--- src/probs/01.js
+++ src/probs/01.js
@@ -1,13 +1,13 @@
 // https://projecteuler.net/problem=1
-exports.solve = function (...args) {
-    var limit = args[0] || 1000;
+exports.solve = function(...args) {
+    let limit = args[0] || 1000;
 
-    var sum = 0;
-    for (var i = 1; i < limit; i++) {
+    let sum = 0;
+    for (let i = 1; i < limit; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
     }
 
     return sum;
-}
+};
--- test/01.test.js
+++ test/01.test.js
@@ -1,10 +1,10 @@
-var assert = require('assert');
+let assert = require('assert');
 
 describe('Euler Problem 1 Tests', function() {
   describe('Solve function', function() {
     it('should return 233168 when the input value is 1000', function() {
-      var solver = require('../src/probs/01.js');
-      var result = solver.solve.apply(null, [1000]);
+      let solver = require('../src/probs/01.js');
+      let result = solver.solve.apply(null, [1000]);
       assert.equal(result, 233168);
     });
   });
